49f6b53dc69f051bad0ca7ed85b2789fba8aed40
forhyeonji <ghkdguswl3@gmail.com>
2022-12-16 18:29:25 +0900

    refactoring to mvc pattern_data

---
+++ server/data/tweet.js
@@ -0,0 +1,54 @@
+let tweets = [
+    {
+        id: '1',
+        text: '너무춥다 너무너무 추워 ㅠ',
+        createdAt: Date.now().toString(),
+        name: 'Bob',
+        username: 'bob',
+        url: 'https://cdn.vectorstock.com/i/1000x1000/51/95/businessman-avatar-cartoon-character-profile-vector-25645195.webp',
+    },
+    {
+        id: '2',
+        text: '손이 너무 차가워 ㅠㅠ',
+        createdAt: Date.now().toString(),
+        name: 'Hyeon',
+        username: 'hyeon',
+    },
+];
+
+export function getAll() {
+    return tweets;
+}
+
+export function getByUsername (username){
+    return tweets.filter(t => t.username === username);
+}
+
+export function getById (id){
+    return tweets.find(f => f.id === id);
+}
+
+export function create (text, name, username){
+    const tweet = {
+        id : Date.now().toString(),
+        text,
+        createdAt : new Date(),
+        name,
+        username,
+    };
+    tweets = [tweet, ...tweets];
+    return tweet;
+}
+
+export function update (id, text){
+    const tweet = tweets.find(f => f.id === id);
+    if(tweet){
+        tweet.text = text;
+    }
+    return tweet;
+}
+
+
+export function remove (id){
+    tweets = tweets.filter(t => t.id !== id);
+}
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,23 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
+import * as tweetRepository from '../data/tweet.js'
 
-let tweets = [
-    {
-        id: '1',
-        text: '너무춥다 너무너무 추워 ㅠ',
-        createdAt: Date.now().toString(),
-        name: 'Bob',
-        username: 'bob',
-        url: 'https://cdn.vectorstock.com/i/1000x1000/51/95/businessman-avatar-cartoon-character-profile-vector-25645195.webp',
-    },
-    {
-        id: '2',
-        text: '손이 너무 차가워 ㅠㅠ',
-        createdAt: Date.now().toString(),
-        name: 'Hyeon',
-        username: 'hyeon',
-    },
-];
 const router = express.Router();
 
 // GET /tweets
@@ -26,8 +10,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     const username = req.query.username;
     const data = username
-        ? tweets.filter(t => t.username === username)
-        : tweets;
+        ? tweetRepository.getByUsername(username)
+        : tweetRepository.getAll();
 
     res.status(200).json(data);
 })
@@ -36,7 +20,7 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
-    const tweet = tweets.find(f => f.id === id);
+    const tweet = tweetRepository.getById(id);
     
     if(tweet){
         res.status(200).json(tweet);
@@ -49,15 +33,7 @@ router.get('/:id', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
     const { text, name, username } = req.body
-    const tweet = {
-        id : Date.now().toString(),
-        text,
-        createdAt : new Date(),
-        name,
-        username,
-    };
-
-    tweets = [tweet, ...tweets];
+    const tweet = tweetRepository.create(text, name, username);
     res.status(201).json(tweet);
 })
 
@@ -71,10 +47,10 @@ router.put('/:id', (req, res, next) => {
 
     const id = req.params.id;
     const text = req.body.text;
+
     // 수정하는 대상이 되는 트윗
-    const tweet = tweets.find(f => f.id === id);
+    const tweet = tweetRepository.update(id, text);
     if (tweet){
-        tweet.text = text;
         res.status(200).json(tweet);
     } else {
         res.status(404).send({message : `Tweet id(${id}) not found`});
@@ -87,7 +63,7 @@ router.put('/:id', (req, res, next) => {
 
 router.delete('/:id', (req, res, next) => {
     const id = req.params.id;
-    tweets = tweets.filter(t => t.id !== id);
+    tweetRepository.remove(id);
     res.sendStatus(204);
 });
 
