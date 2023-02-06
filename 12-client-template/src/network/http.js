7826ac5d854baf2f5e9d68d397fca285dd7123d6
forhyeonji <ghkdguswl3@gmail.com>
2023-02-06 18:07:00 +0900

    axios-retry 요청

--- 12-client-template/src/network/http.js
+++ 12-client-template/src/network/http.js
@@ -1,13 +1,34 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 
+const defaultRetryConfig = {
+  retries: 5,
+  initialDelayMs: 100,
+};
 export default class HttpClient {
-  constructor(baseURL, authErrorEventBus, getCsrfToken) {
+  constructor(
+    baseURL,
+    authErrorEventBus,
+    getCsrfToken,
+    config = defaultRetryConfig
+  ) {
     this.authErrorEventBus = authErrorEventBus;
     this.getCsrfToken = getCsrfToken;
     this.client = axios.create({
       baseURL: baseURL,
       headers: { 'Content-Type': 'application/json' },
       withCredentials: true,
+    });
+    axiosRetry(this.client, {
+      retries: config.retries,
+      retryDelay: (retry) => {
+        const delay = Math.pow(2, retry) * config.initialDelayMs; // 100, 200, 400, 800, 1600
+        const jitter = delay * 0.1 * Math.random(); // 10, 20, 40, ... 160
+        return delay + jitter;
+      },
+      retryCondition: (err) =>
+        axiosRetry.isNetworkOrIdempotentRequestError(err) ||
+        err.response.status === 429,
     });
   }
 
